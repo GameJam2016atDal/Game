7328a0a68074eef417dc38acd0f76a3439ae4af5
Yaxin Cheng <yx299612@dal.ca>
2016-10-11 10:30:10 -0300

    Separate sticks and platforms

--- game.py
+++ game.py
@@ -1,17 +1,10 @@
 #!/Users/Cheng/Developer/Python/PyGame/venv/bin/python
 import pygame, os
-from pygame.locals import *
+#from pygame.locals import *
 from pygame.sprite import Sprite
 from pygame.image import load
 
 BGCOLOUR = (249, 250, 255)
-
-pygame.init()
-screenSize = (1440, 850)
-fullScreen = False
-screen = pygame.display.set_mode(screenSize, fullScreen)
-cwd = os.getcwd()
-screen.fill(BGCOLOUR)
 
 class platform(Sprite):
 	def __init__(self, img):
@@ -21,8 +14,8 @@ class platform(Sprite):
 
 	@staticmethod
 	def generatePlatforms():
-		imgs = ['platform1', 'layer2_l', 'layer2', 'pillar_l', 'pillar_r', 'shortStage', 'shortStage', 'shortStage', 'shortStage', 'topStage', 'topStage', 'sticks', 'sticks', 'bottomSticks']
-		coordinates = [(0, 800), (270, 690), (780, 690), (270, 700), (1100, 700), (300, 500), (840, 500), (300, 350), (840, 350), (170, 200), (780, 200), (0, 690), (1330, 690), (350, 720)]
+		imgs = ['platform1', 'layer2_l', 'layer2', 'pillar_l', 'pillar_r', 'shortStage', 'shortStage', 'shortStage', 'shortStage', 'topStage', 'topStage']
+		coordinates = [(0, 800), (270, 690), (780, 690), (270, 700), (1100, 700), (300, 500), (840, 500), (300, 350), (840, 350), (170, 200), (780, 200)]
 		platFormList = list()
 		for (eachImg, eachCoordinate) in zip(imgs, coordinates):
 			plat = platform(img = eachImg)
@@ -30,25 +23,32 @@ class platform(Sprite):
 			platFormList.append(plat)
 		return platFormList
 
-spriteList = pygame.sprite.Group()
-plats = platform.generatePlatforms()
-for plat in plats:
-	spriteList.add(plat)
-spriteList.draw(screen)
+	@staticmethod
+	def generateSticks():
+		imgs = ['sticks', 'sticks', 'bottomSticks']
+		coordinates = [(0, 690), (1330, 690), (350, 720)]
+		stickList = list()
+		for (eachImg, eachCoordinate) in zip(imgs, coordinates):
+			stick = platform(img = eachImg)
+			stick.rect.x, stick.rect.y = eachCoordinate
+			stickList.append(stick)
+		return stickList
 
 class player(Sprite):
-	def __init__(self):
+	def __init__(self, platforms, sticks):
 		super().__init__()
 		self.image = pygame.Surface([50, 100])
 		self.image.fill((0, 255, 0))
 		self.rect = self.image.get_rect()
 		self.xSpeed = 0
 		self.ySpeed = 0
+		self.platforms = platforms
+		self.sticks = sticks
 
 	def update(self):
 		self.calc_grav()
 		self.rect.x += self.xSpeed
-		block_hit_list = pygame.sprite.spritecollide(self, spriteList, False)
+		block_hit_list = pygame.sprite.spritecollide(self, self.platforms, False)
 		for block in block_hit_list:
 			if self.xSpeed > 0:
 				self.rect.right = block.rect.left
@@ -56,7 +56,7 @@ class player(Sprite):
 				self.rect.left = block.rect.right
 
 		self.rect.y += self.ySpeed
-		block_hit_list = pygame.sprite.spritecollide(self, spriteList, False)
+		block_hit_list = pygame.sprite.spritecollide(self, self.platforms, False)
 		for block in block_hit_list:
 			if self.ySpeed > 0:
 				self.rect.bottom = block.rect.top
@@ -75,7 +75,7 @@ class player(Sprite):
 
 	def jump(self):
 		self.rect.y += 1.3
-		platform_hit_list = pygame.sprite.spritecollide(self, spriteList, False)
+		platform_hit_list = pygame.sprite.spritecollide(self, self.platforms, False)
 		self.rect.y -= 1.3
 		if len(platform_hit_list) > 0 or self.rect.bottom >= 850:
 			self.ySpeed = -10
@@ -89,41 +89,64 @@ class player(Sprite):
 	def stop(self):
 		self.xSpeed = 0
 
-Player = player()
+def main():
+	pygame.init()
+	screenSize = (1440, 850)
+	fullScreen = False
+	screen = pygame.display.set_mode(screenSize, fullScreen)
+	cwd = os.getcwd()
+	screen.fill(BGCOLOUR)
 
-Player.rect.x = 30
-Player.rect.y = 500
-plat.player = Player
-spriteList.add(plat)
-activeList = pygame.sprite.Group()
-activeList.add(Player)
-clock = pygame.time.Clock()
-gaming = True
+	platformList = pygame.sprite.Group()
+	plats = platform.generatePlatforms()
+	for plat in plats:
+		platformList.add(plat)
+	platformList.draw(screen)
 
-while gaming:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			gaming = False
-			break
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_ESCAPE:
+	stickList = pygame.sprite.Group()
+	sticks = platform.generateSticks()
+	for stick in sticks:
+		stickList.add(stick)
+	stickList.draw(screen)
+
+	Player = player(platforms = platformList, sticks = stickList)
+
+	Player.rect.x = 30
+	Player.rect.y = 500
+	plat.player = Player
+	platformList.add(plat)
+	activeList = pygame.sprite.Group()
+	activeList.add(Player)
+	clock = pygame.time.Clock()
+	gaming = True
+
+	while gaming:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
 				gaming = False
 				break
-			if event.key == pygame.K_LEFT:
-				Player.go_left()
-			if event.key == pygame.K_RIGHT:
-				Player.go_right()
-			if event.key == pygame.K_UP:
-				Player.jump()
-		if event.type == pygame.KEYUP:
-			if event.key == pygame.K_LEFT and Player.xSpeed < 0:
-				Player.stop()
-			if event.key == pygame.K_RIGHT and Player.xSpeed > 0:
-				Player.stop()
-	screen.fill(BGCOLOUR)
-	spriteList.draw(screen)
-	activeList.update()
-	activeList.draw(screen)
-	clock.tick(60)
-	pygame.display.flip()
-pygame.quit()
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_ESCAPE:
+					gaming = False
+					break
+				if event.key == pygame.K_LEFT:
+					Player.go_left()
+				if event.key == pygame.K_RIGHT:
+					Player.go_right()
+				if event.key == pygame.K_UP:
+					Player.jump()
+			if event.type == pygame.KEYUP:
+				if event.key == pygame.K_LEFT and Player.xSpeed < 0:
+					Player.stop()
+				if event.key == pygame.K_RIGHT and Player.xSpeed > 0:
+					Player.stop()
+		screen.fill(BGCOLOUR)
+		platformList.draw(screen)
+		activeList.update()
+		activeList.draw(screen)
+		clock.tick(60)
+		pygame.display.flip()
+	pygame.quit()
+
+if __name__ == '__main__':
+	main()
